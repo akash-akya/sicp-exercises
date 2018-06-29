2ea7e5709324553b7b8dc5256f49844303fb10bb
akash-akya <akashh246@gmail.com>
2018-06-29 16:07:50 +0530

    Chapter 3 started

---
+++ chapter_3/utils.scm
@@ -0,0 +1,60 @@
+#lang sicp
+(#%require racket/base)
+
+(define (square x) (* x x))
+
+(define (cube x) (* x x x))
+
+;; helper functions
+(define (filter predicate list)
+  (cond ((null? list) nil)
+        ((predicate (car list))
+         (cons (car list)
+               (filter predicate (cdr list))))
+        (else
+         (filter predicate (cdr list)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low
+            (enumerate-interval
+             (+ low 1)
+             high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+
+(define (success lhs rhs)
+  (display "Passed..."))
+
+(define (failure lhs rhs)
+  (display "Failed!  ")
+  (display lhs)
+  (display "  =  ")
+  (display rhs))
+
+;; test utils
+(define-syntax-rule (test-equal lhs rhs)
+  (let ((qlhs (quote lhs))
+        (qrhs (quote rhs)))
+    (cond [(equal? lhs rhs) (success qlhs qrhs)]
+          [else (failure qlhs qrhs)])
+    (newline)))
+
+
+(#%provide flatmap)
+(#%provide filter)
+(#%provide accumulate)
+(#%provide square)
+(#%provide cube)
+(#%provide test-equal)
